92a090b6af3de69e8e41f9d01a943488200c597b
Michal Cierniak <cierniak@users.noreply.github.com>
2015-11-08 20:15:45 -0800

    Fact is finally computing a factorial

--- tests/fac/fac.go
+++ tests/fac/fac.go
@@ -2,13 +2,13 @@ package fac
 
 import "gowasm/rt/wasm"
 
-//wasm:assert_return (invoke "Fact" (i64.const 3)) (i64.const 3)
-//wasm:assert_return (invoke "Fact" (i64.const 0)) (i64.const 13)
+//wasm:assert_return (invoke "Fact" (i64.const 0)) (i64.const 1)
+//wasm:assert_return (invoke "Fact" (i64.const 3)) (i64.const 6)
 func Fact(n int64) int64 {
 	if n == 0 {
-		return 13
+		return 1
 	}
-	return n
+	return n * Fact(n-1)
 }
 
 //wasm:invoke (invoke "PrintAll" (i64.const 3))
