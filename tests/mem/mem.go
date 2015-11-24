e7a7906f8dd975ba1ed212c12fae4a2ae4c1c7a2
Michal Cierniak <cierniak@users.noreply.github.com>
2015-11-24 14:04:52 -0800

    Moved stable tests to mem.go

--- tests/mem/mem.go
+++ tests/mem/mem.go
@@ -52,3 +52,29 @@ func PtrToInt32() {
 	i32 := *xp
 	wasm.Print_int32(i32)
 }
+
+//wasm:invoke (invoke "Peek32" (i32.const 0))
+func Peek32(addr uintptr) int32 {
+	u1 := unsafe.Pointer(addr)
+	p := (*int32)(u1)
+	i32 := *p
+	return i32
+}
+
+//wasm:invoke (invoke "TestPeek32")
+func TestPeek32() {
+	p := &Point{}
+	p.x = int32(17)
+	xp := &p.x
+	u1 := unsafe.Pointer(xp)
+	u := uintptr(u1)
+	i32 := Peek32(u)
+	wasm.Print_int32(i32)
+}
+
+//wasm:invoke (invoke "DumpMemory" (i32.const 0) (i32.const 100))
+func DumpMemory(start, end uintptr) {
+	for i := start; i < end; i = i + 4 {
+		wasm.Print_int32(Peek32(i))
+	}
+}
--- tests/newstuff/newstuff.go
+++ tests/newstuff/newstuff.go
@@ -1,35 +1,10 @@
 package newstuff
 
-import "unsafe"
 import "gowasm/rt/wasm"
 
-type Point struct {
-	x int32
-	y int32
-}
+var global32 int32 = 15
 
-//wasm:invoke (invoke "Peek32" (i32.const 0))
-func Peek32(addr uintptr) int32 {
-	u1 := unsafe.Pointer(addr)
-	p := (*int32)(u1)
-	i32 := *p
-	return i32
-}
-
-//wasm:invoke (invoke "TestPeek32")
-func TestPeek32() {
-	p := &Point{}
-	p.x = int32(17)
-	xp := &p.x
-	u1 := unsafe.Pointer(xp)
-	u := uintptr(u1)
-	i32 := Peek32(u)
-	wasm.Print_int32(i32)
-}
-
-//wasm:invoke (invoke "DumpMemory" (i32.const 0) (i32.const 100))
-func DumpMemory(start, end uintptr) {
-	for i := start; i < end; i = i + 4 {
-		wasm.Print_int32(Peek32(i))
-	}
+//wasm:invoke (invoke "TestGlobalVar")
+func TestGlobalVar() {
+	wasm.Print_int32(global32)
 }
