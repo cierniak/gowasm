2d44908fac1d09243225aa9733f9a7bdcc80ed78
Michal Cierniak <cierniak@users.noreply.github.com>
2015-12-04 14:28:27 -0800

    Moved simple call_indirect cases to stable tests

--- tests/i32/i32.go
+++ tests/i32/i32.go
@@ -79,3 +79,29 @@ func UntypedLiteral() int32 {
 	j := 1
 	return int32(i + j)
 }
+
+// Indrect calls
+type IntFunc func() int
+type IntFunc2 func(int, int) int
+
+func Twelve() int {
+	return 12
+}
+
+func Sum(a, b int) int {
+	return a + b
+}
+
+//wasm:assert_return (invoke "Test1") (i32.const 12)
+func Test1() int {
+	var f IntFunc
+	f = Twelve
+	return f()
+}
+
+//wasm:assert_return (invoke "Test2") (i32.const 17)
+func Test2() int {
+	var f1 IntFunc2
+	f1 = Sum
+	return f1(10, 7)
+}
