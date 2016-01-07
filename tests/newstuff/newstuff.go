cf356d7988a5794f1b17c6b79b822593c3bc2fe8
Michal Cierniak <cierniak@users.noreply.github.com>
2016-01-07 13:22:01 -0800

    Added a test case for puts

--- tests/newstuff/newstuff.go
+++ tests/newstuff/newstuff.go
@@ -1,6 +1,7 @@
 package newstuff
 
-//import "gowasm/rt/wasm"
+import "gowasm/rt/wasm"
+
 //import "gowasm/rt/gc"
 //import "unsafe"
 
@@ -8,10 +9,10 @@ package newstuff
 //	return *p
 //}
 
-//wasm:assert_return (invoke "TestArray7") (i32.const 65)
-func TestArray7() byte {
+//wasm:assert_return (invoke "TestPuts") (i32.const 0)
+func TestPuts() int {
 	a := [...]byte{'h', 'e', 'l', 'l', 'o', 0}
-	p := &a[2]
-	*p = 65
-	return *p
+	p := &a[0]
+	wasm.Puts(p)
+	return 0
 }
